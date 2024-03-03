6cc85aaeda3a02183b0ac2a45c729ddf91f713fc
CHIRAG JAIN <chirag.jain1@somaiya.edu>
2024-03-02 18:59:34 -0800

    Update test.py

--- Functions/test.py
+++ Functions/test.py
@@ -124,7 +124,7 @@ class Motion():
         self.starting_position()
         #self.perception.run(color,5,my_camera)
         resnet = self.go_to_location((473, 106-2, 12), -90, -90, 0)
-        time.sleep(resnet[2]/1000) 
+        #time.sleep(resnet[2]/1000) 
         time.sleep(1)
         self.grippers(False)
         self.go_to_location((273, 106-2, 12), -90, -90, 0)
