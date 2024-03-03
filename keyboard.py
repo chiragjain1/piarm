188eb45774ce917e20c300183eedc16bbd745217
CHIRAG JAIN <chirag.jain1@somaiya.edu>
2024-03-02 17:43:16 -0800

    Update keyboard.py

--- keyboard.py
+++ keyboard.py
@@ -15,10 +15,6 @@ cmd = {
     }
 
 step_width = 10
-key_map = {"PSB_CROSS":2, "PSB_CIRCLE":1, "PSB_SQUARE":3, "PSB_TRIANGLE":0,
-        "PSB_L1": 4, "PSB_R1":5, "PSB_L2":6, "PSB_R2":7,
-        "PSB_SELECT":8, "PSB_START":9, "PSB_L3":10, "PSB_R3":11};
-action_map = ["CROSS", "CIRCLE", "", "SQUARE", "TRIANGLE", "L1", "R1", "L2", "R2", "SELECT", "START", "", "L3", "R3"]
 
 os.environ["SDL_VIDEODRIVER"] = "dummy"
 pygame.init()
@@ -32,68 +28,35 @@ win.fill((255,255,255))
 win.blit(text_surface, (40,100))
 keys = pygame.key.get_pressed()
 msg = {}
-pygame.joystick.init()
-if pygame.joystick.get_count() > 0:
-    js=pygame.joystick.Joystick(0)
-    js.init()
-    jsName = js.get_name()
-    print("Name of the joystick:", jsName)
-    jsAxes=js.get_numaxes()
-    print("Number of axif:",jsAxes)
-    jsButtons=js.get_numbuttons()
-    print("Number of buttons:", jsButtons);
-    jsBall=js.get_numballs()
-    print("Numbe of balls:", jsBall)
-    jsHat= js.get_numhats()
-    print("Number of hats:", jsHat)
 
-connected = True
 change = [500,500,136,931,795,500]
 while True:
-    # if os.path.exists("/dev/input/js0") is True:
-    #     if connected is False:
-    #         jscount =  pygame.joystick.get_count()
-    #         if jscount > 0:
-    #             try:
-    #                 js=pygame.joystick.Joystick(0)
-    #                 js.init()
-    #                 connected = True
-    #             except Exception as e:
-    #                 print(e)
-    #         else:
-    #             pygame.joystick.quit()
-    # else:
-    #     if connected is True:
-    #         connected = False
-    #         js.quit()
-    #         pygame.joystick.quit()
-    if connected is True:
-        pygame.event.pump()
-        keys = pygame.key.get_pressed()
-        try:
-            if keys[pygame.K_a] :
-                print("hi")
-                change[0] -= step_width
-                change[0] = 0 if change[0] < 0 else change[0]
-                cmd["params"] = [20, 1, 1, change[0]]
-                r = requests.post(url, json = cmd).json()
-            if keys[pygame.K_RIGHT] :
-                change[0] += step_width
-                change[0] = 1000 if change[0] > 1000 else change[0]
-                cmd["params"] = [20, 1, 1, change[0]]
-                r = requests.post(url, json = cmd).json()
-            if keys[pygame.K_UP] :
-                change[1] -= step_width
-                change[1] = 0 if change[1] < 0 else change[1]
-                cmd["params"] = [20, 1, 2, change[1]]
-                r = requests.post(url, json = cmd).json()
-            if keys[pygame.K_DOWN] :
-                change[1] += step_width
-                change[1] = 1000 if change[1] > 1000 else change[1]
-                cmd["params"] = [20, 1, 2, change[1]]
-                r = requests.post(url, json = cmd).json()
-            # if js.get_button(key_map["PSB_R2"]) :
-            #     change[2] += step_width
+    pygame.event.pump()
+    keys = pygame.key.get_pressed()
+    try:
+        if keys[pygame.K_a] :
+            print("hi")
+            change[0] -= step_width
+            change[0] = 0 if change[0] < 0 else change[0]
+            cmd["params"] = [20, 1, 1, change[0]]
+            r = requests.post(url, json = cmd).json()
+        if keys[pygame.K_RIGHT] :
+            change[0] += step_width
+            change[0] = 1000 if change[0] > 1000 else change[0]
+            cmd["params"] = [20, 1, 1, change[0]]
+            r = requests.post(url, json = cmd).json()
+        if keys[pygame.K_UP] :
+            change[1] -= step_width
+            change[1] = 0 if change[1] < 0 else change[1]
+            cmd["params"] = [20, 1, 2, change[1]]
+            r = requests.post(url, json = cmd).json()
+        if keys[pygame.K_DOWN] :
+            change[1] += step_width
+            change[1] = 1000 if change[1] > 1000 else change[1]
+            cmd["params"] = [20, 1, 2, change[1]]
+            r = requests.post(url, json = cmd).json()
+        # if js.get_button(key_map["PSB_R2"]) :
+        #     change[2] += step_width
             #     change[2] = 1000 if change[2] > 1000 else change[2]
             #     cmd["params"] = [20, 1, 3, change[2]]
             #     r = requests.post(url, json = cmd).json() 
@@ -196,7 +159,7 @@ while True:
             #     change = [500,500,136,931,795,500]
             #     cmd["params"] =  [1000, 6, 1, 500, 2, 500, 3, 136, 4, 931, 5, 795, 6, 500]
             #     r = requests.post(url, json = cmd).json()                
-        except Exception as e:
-            print(e)
-            connected = False          
+    except Exception as e:
+        print(e)         
     time.sleep(0.06)
+    pygame.display.flip()
